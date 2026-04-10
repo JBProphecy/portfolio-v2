6d27afbd42ae09f9e86c8e5876db2ec58b0abe52
Jack <jackbpiatt@gmail.com>
2026-04-10 15:49:49 -0400

    unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import { WA } from './app/windows/WA.tsx'
-import { WB } from './app/windows/WB.tsx'
-import { WC } from './app/windows/WC.tsx'
 import { WD } from './app/windows/WD.tsx'
-import { PA } from './app/printer/PA.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
