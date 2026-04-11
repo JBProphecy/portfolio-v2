4a9c9d727930773bd67fd76df610c16ec9aa78aa
Jack <jackbpiatt@gmail.com>
2026-04-10 21:59:12 -0400

    darker

--- src/app/windows/WD.tsx
+++ src/app/windows/WD.tsx
@@ -7,19 +7,55 @@ import { SlideHero } from "../slides/hero";
 import { SlideProjectNaturalDisasters, SlideProjectNaturalDisastersResponsibilities } from "../slides/project-natural-disasters";
 import { SlideProjectWebServerLogEvents, SlideProjectWebServerLogEventsResponsibilities } from "../slides/project-web-server-log-events";
 
+function SlideBorder({ children }: { children?: React.ReactNode }) {
+  return (
+    <div style={{ border: "0.25rem solid #808080", borderRadius: `${0.25 + 0.5}rem` }}>
+      {children}
+    </div>
+  )
+}
+
 export function WD() {
   return (
     <SlideDeckOuter>
       <SlideDeckInner>
-        <SlideHero />
-        <SlideAboutMe />
-        <SlideEducation />
-        <SlidePieChartOne />
-        <SlidePieChartTwo />
-        <SlideProjectWebServerLogEvents />
-        <SlideProjectWebServerLogEventsResponsibilities />
-        <SlideProjectNaturalDisasters />
-        <SlideProjectNaturalDisastersResponsibilities />
+
+        <SlideBorder>
+          <SlideHero />
+        </SlideBorder>
+
+        <SlideBorder>
+          <SlideAboutMe />
+        </SlideBorder>
+
+        <SlideBorder>
+          <SlideEducation />
+        </SlideBorder>
+
+        <SlideBorder>
+          <SlidePieChartOne />
+        </SlideBorder>
+
+        <SlideBorder>
+          <SlidePieChartTwo />
+        </SlideBorder>
+
+        <SlideBorder>
+          <SlideProjectWebServerLogEvents />
+        </SlideBorder>
+
+        <SlideBorder>
+          <SlideProjectWebServerLogEventsResponsibilities />
+        </SlideBorder>
+
+        <SlideBorder>
+          <SlideProjectNaturalDisasters />
+        </SlideBorder>
+
+        <SlideBorder>
+          <SlideProjectNaturalDisastersResponsibilities />
+        </SlideBorder>
+
       </SlideDeckInner>
     </SlideDeckOuter>
   )
